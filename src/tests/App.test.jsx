d55a057a25bbd836cb3f276581f768a6b980f213
jmo5896 <justin.m.moore5896@gmail.com>
2024-03-07 14:19:05 -0700

    test github actions

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,10 +1,16 @@
-import { render } from '@testing-library/react';
-import App from '../App';
+import { render, screen } from "@testing-library/react";
+import App from "../App";
 
-describe('<App/> component', () => {
-
-  it('renders', () => {
+describe("<App/> component", () => {
+  it("renders", () => {
     render(<App />);
   });
 
+  it("header component check", () => {
+    render(<App />);
+
+    const header = screen.getByText("LOTR Project");
+
+    expect(header).toBeDefined();
+  });
 });
